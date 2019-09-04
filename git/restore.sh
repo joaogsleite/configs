30c81ed25732428f1053842f9fa6e443032b03ba
joaogsleite <joao.leite@isobar.com>
2019-09-04 11:18:52 +0100

    updated git restore script

--- git/restore.sh
+++ git/restore.sh
@@ -2,7 +2,4 @@
 
 cp ./gitignore ~/.gitignore
 cp ./gitconfig ~/.gitconfig
-git config --global core.excludesfile '~/.gitignore'
-git config --global user.email "joaogsleite@gmail.com"
-git config --global user.name "joaogsleite"
-git config --global core.editor "vim"
+
