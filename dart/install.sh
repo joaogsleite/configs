a710a0e29c16acb08d046f77cae9f71b258f5da1
joaogsleite <joao.leite@isobar.com>
2020-01-27 16:36:21 +0000

    dart

--- dart/install.sh
+++ dart/install.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
 
 # update repos
-sudo apt update
-sudo apt install -y apt-transport-https
-sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 
 # install dart
 sudo apt update
 sudo apt install -y dart
+
