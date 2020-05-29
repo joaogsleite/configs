f2eeee1a46f6288490e3e3d45221851cc8e2217d
joaogsleite <joao.leite@isobar.com>
2020-05-29 12:50:25 +0100

    nothing to say gnome extension

---
+++ gnome-extensions/nothing-to-say/restore.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+dconf write /org/gnome/shell/extensions/nothing-to-say/keybinding-toggle-mute '["F4", "Pause"]'
+
---
+++ master-pdf/install.sh
@@ -0,0 +1,7 @@
+#/!usr/bin/env bash
+
+wget -O master-pdf.deb http://code-industry.net/public/master-pdf-editor-4.3.89_qt5.amd64.deb
+
+sudo dpkg -i master-pdf.deb
+
+sudo dpkg -i master-pdf.deb
