c9994b422c5bc60737627f69abb49526c6508a32
joaogsleite <joao.leite@isobar.com>
2020-07-24 14:36:06 +0100

    python2 on ubuntu 20

--- python/install.sh
+++ python/install.sh
@@ -3,4 +3,6 @@
 sudo apt update
 
 sudo apt install -y python3-pip
-sudo apt install -y python-pip
+sudo apt install -y python2
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
