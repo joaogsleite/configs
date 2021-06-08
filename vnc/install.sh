03373db25238969c14e44c805297ebbbbe7e10de
João Leite <joao.leite@isobar.com>
2021-06-08 22:49:31 +0100

    vnc client

--- vnc/install.sh
+++ vnc/install.sh
@@ -3,4 +3,7 @@
 sudo apt install -y vino
 
 # vnc client
-sudo apt install -y xtightvncviewer
+sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
+sudo apt update -y
+sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
+
