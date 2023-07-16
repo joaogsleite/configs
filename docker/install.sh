e1a8d2a73293fdca197b526eb75ce024135eadc4
João Leite <Joao.Leite@merkle.com>
2023-07-16 18:10:42 +0100

    orbstack

--- docker/install.sh
+++ docker/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-  brew install --cask podman-desktop
-  brew install docker-compose
+  brew install --cask orbstack 
 else
   sudo apt install -y docker.io
   sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
--- docker/restore.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo mkdir -p /usr/local/bin
-sudo cp ./docker /usr/local/bin/docker
-sudo chmod +x /usr/local/bin/docker
-
