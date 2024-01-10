0c4744e2ce5a71fc75eaeeeb1d46c1e26babc453
João Leite <Joao.Leite@merkle.com>
2024-01-10 22:34:02 +0000

    zsh and spotify

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,7 +6,7 @@ else
   sudo apt install -y zsh
 fi
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
