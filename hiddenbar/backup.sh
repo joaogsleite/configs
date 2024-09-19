fc7e177c507b1a59f492b1fcbdebc068b5727367
João Leite <Joao.Leite@merkle.com>
2024-09-19 11:08:34 +0100

    jordanbaird-ice

--- hiddenbar/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  cp ~/Library/Containers/com.dwarvesv.minimalbar/Data/Library/Preferences/com.dwarvesv.minimalbar.plist ./config.plist
-fi
--- hiddenbar/install.sh
+++ hiddenbar/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  brew install --cask hiddenbar
+  brew install --cask jordanbaird-ice
 else
   echo "not needed on Linux"
 fi
