8c70c31bde8b6889f8e16ceae224c4074b324a92
João Leite <Joao.Leite@merkle.com>
2024-12-05 14:22:03 +0000

    pnpm instead of npm

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# install nvm
-REPO="nvm-sh/nvm"
-VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r ".tag_name")
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$VERSION/install.sh | bash
-
 # install pnpm
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+wget -qO- https://get.pnpm.io/install.sh | sh -
 
