c6e257ebe0ff73cebdcc60f77c6f1b565f765474
João Leite <Joao.Leite@merkle.com>
2025-02-11 11:50:41 +0000

    browsers

---
+++ brave/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install --cask brave-browser
+else
+  # todo
+fi
---
+++ microsoft-edge/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install --cask microsoft-edge
+else
+  # todo
+fi
