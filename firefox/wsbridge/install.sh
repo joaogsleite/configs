47bf1ce306707efcb3023e97c17312242a738b01
Joao Leite <joaogsleite@gmail.com>
2025-09-11 20:33:02 +0100

    wsbridge service

--- firefox/wsbridge/install.sh
+++ firefox/wsbridge/install.sh
@@ -3,5 +3,7 @@
 pnpm install
 mkdir -p ~/Library/LaunchAgents
 cp com.joaogsleite.wsbridge.plist ~/Library/LaunchAgents/com.joaogsleite.wsbridge.plist
+launchctl stop com.joaogsleite.wsbridge || true
+launchctl unload ~/Library/LaunchAgents/com.joaogsleite.wsbridge.plist || true
 launchctl load ~/Library/LaunchAgents/com.joaogsleite.wsbridge.plist
 launchctl start com.joaogsleite.wsbridge
